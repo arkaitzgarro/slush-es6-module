272d267a247b4a64fae588421809d1975b89b44d
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-01-27 23:08:14 +0100

    style(template): Fix coding style

--- templates/test/unit/index.spec.js
+++ templates/test/unit/index.spec.js
@@ -5,9 +5,7 @@
 import <%= appNameCamel %> from '../../dist/index'
 
 describe('appName test:', () => {
-
   it('instance is created', () => {
     expect(<%= appNameCamel %>).to.not.be.undefined
   })
-
 })
